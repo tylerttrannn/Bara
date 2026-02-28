0fa4650b1ad8494c598a8af6f1f8d4ec6717e97f
Tyler Tran <77897545+tylerttrannn@users.noreply.github.com>
2026-02-27 22:29:12 -0800

    Initial Commit

---
+++ Bara/BaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaraApp.swift
+//  Bara
+//
+//  Created by Tyler Tran on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct BaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BaraTests/BaraTests.swift
@@ -0,0 +1,17 @@
+//
+//  BaraTests.swift
+//  BaraTests
+//
+//  Created by Tyler Tran on 2/27/26.
+//
+
+import Testing
+@testable import Bara
+
+struct BaraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
